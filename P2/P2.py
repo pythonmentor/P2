b34b102a1d483d56ebe625aa64abb21c299b1882
Maxime <maxim.lescure63@gmail.com>
2020-12-03 14:24:55 +0100

    Mise en odre de ma liste

--- P2/P2.py
+++ P2/P2.py
@@ -8,24 +8,33 @@ url ="http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
 #Initialisation de la connection de la page que nous voulons récuperer les informations.
 response = requests.get(url)
 if response.ok:
-   soup = BeautifulSoup(response.text, 'html.parser')
-   title = soup.find('title')
-   infos.append(title.string)
+   soup = BeautifulSoup(response.text, 'html.parser') 
+   infos.append(url) #On ajoute l'URL
+   infos.append(soup.find('li', {'class':'active' } ).string) #On ajoute le titre
+   infos.append(soup.find('ul', {"class":"breadcrumb"}).findAll('li')[2].find('a').string) #On ajoute la catégorie
+
+
+
+   #Ajout des autres informations qui sont dans les <td> en discriminant les <tr> contenant Product type et Tax. 
+
+   trs = soup.findAll('tr')
+   for tr in trs:     
+        th = tr.find('th')
+        td = tr.find('td')
+        if (th.string != "Product Type") and (th.string !="Tax"):
+           infos.append(td.string)
+           
+
+  
+   infos.append(soup.findAll('p')[3]) #Ajout de la déscription
+
+
+   infos.append(soup.find('div',{'class':'item active'}).find('img')['src']) #Ajout du lien de l'image 
    
-   ths = soup.findAll('tr')
-   for th in ths:
-       th = th.find('td')
-       infos.append(th.string)
-
-   dscs = soup.findAll('p')
-   infos.append(dscs[3])
-
-   infos.append(soup.find('div',{'class':'item active'}).find('img')['src'])
-   
-
-   print(infos)
    
 with open("information.csv","w",newline="") as f:
     ecriture = csv.writer(f)
     ecriture.writerow(['product_page_url', 'universal_product_code', 'title', 'price_including_tax', 'price_excluding_tax', 'number_available', 'product_description', 'category', 'review_rating', 'image_url'])
-    ecriture.writerow([url, infos[0], infos[1], infos[2], infos[3], infos[4], infos[5]])
+    ecriture.writerow([infos[0], infos[3], infos[1], infos[4], infos[5], infos[6], infos[8], infos[2], infos[7], infos[9]])
+
+    [print(str([i]) + '\n') for i in infos]
